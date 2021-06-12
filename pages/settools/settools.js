67c6ae27967b3dc566dad55cdbcf12241ee601eb
arsize <arsizeof3416@gmail.com>
2021-06-12 18:54:08 +0800

    feat:loginout

--- pages/settools/settools.js
+++ pages/settools/settools.js
@@ -1,39 +1,42 @@
-var app = getApp()
+const app = getApp();
+import {
+    globalData
+} from "../../utils/global";
+import {
+    HTTP
+} from "../../utils/server";
 
 Page({
     data: {
         baseUrlImg: app.globalData.baseUrlImg,
+        token: "",
+        userId: ""
     },
-    //options(Object)
-    onLoad: function(options){
-        
+    onShow() {
+        let logindata = wx.getStorageSync('logindata')
+        let token = logindata.token
+        let userId = logindata.userInfo.userId
+        this.data.token = token
+        this.data.userId = userId
     },
-    onReady: function(){
-        
-    },
-    onShow: function(){
-        
-    },
-    onHide: function(){
+    loginout() {
+        HTTP({
+            url: `wxLoginOut?token=${this.data.token}&&userId=${this.data.userId}`,
+            methods: "post",
+        }).then(res => {
+            console.log('res', res)
+            wx.showToast({
+                title: `退出登录`,
+                icon: 'success',
+                duration: 2000
+            })
+            setTimeout(() => {
+                wx.redirectTo({
+                    url: "/pages/index/index"
+                })
+            }, 1000)
 
-    },
-    onUnload: function(){
-
-    },
-    onPullDownRefresh: function(){
-
-    },
-    onReachBottom: function(){
-
-    },
-    onShareAppMessage: function(){
-
-    },
-    onPageScroll: function(){
-
-    },
-    //item(index,pagePath,text)
-    onTabItemTap:function(item){
+        })
 
     }
 });
