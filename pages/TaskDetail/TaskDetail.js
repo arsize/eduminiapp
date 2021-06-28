172a0c8280dafd5f0ff31c1c8841727bcb9de176
arsize <arsizeof3416@gmail.com>
2021-06-28 14:03:22 +0800

    fix

---
+++ pages/TaskDetail/TaskDetail.js
@@ -0,0 +1,8 @@
+var app = getApp()
+Page({
+    data: {
+        baseUrlImg: app.globalData.baseUrlImg,
+        banner_background: ['http://121.4.213.7/img/index_project_covers.jpg'],
+    },
+    //options(Object)
+});
